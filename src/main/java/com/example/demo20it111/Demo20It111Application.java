f4db38ecc44f13cda5b339605ac1b267cbb3779f
unknown <20it111@charusat.edu.in>
2022-09-06 13:09:02 +0530

    First Commit

---
+++ src/main/java/com/example/demo20it111/Demo20It111Application.java
@@ -0,0 +1,13 @@
+package com.example.demo20it111;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo20It111Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo20It111Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo20it111/Demo20It111ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo20it111;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo20It111ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
